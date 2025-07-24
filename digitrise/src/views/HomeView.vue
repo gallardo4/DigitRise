89bcb61c27b33391e56442962af9ddd157805752
gallardo4 <arnaugaco4@gmail.com>
2025-07-24 13:38:50 +0200

    update home order

--- digitrise/src/views/HomeView.vue
+++ digitrise/src/views/HomeView.vue
@@ -12,21 +12,21 @@
 
     <div class="info-extra">
       <div>
-        <h3 class="tituloGrande">Equipo de DigitRise</h3>
-        <p class="texto">
-          Conoce al equipo detrás de <strong>DigitRise</strong>, apasionados por la tecnología y la innovación. Estamos aquí para ayudarte a
-          llevar tu negocio al siguiente nivel con soluciones de IA personalizadas.
-        </p>
-        <RouterLink to="/equipo"><button class="btn" onclick="this.blur()">Conoce al equipo</button></RouterLink>
-      </div>
-
-      <div>
         <h3 class="tituloGrande">Proyectos llevados a cabo</h3>
         <p class="texto">
           Descubre cómo hemos ayudado a empresas como la tuya a transformar su presencia digital y optimizar sus procesos
           con nuestras soluciones de IA. Cada proyecto es una historia de éxito.
         </p>
         <RouterLink to="/proyectos"><button class="btn" onclick="this.blur()">Descubre los proyectos</button></RouterLink>
+      </div>
+
+      <div>
+        <h3 class="tituloGrande">Equipo de DigitRise</h3>
+        <p class="texto">
+          Conoce al equipo detrás de <strong>DigitRise</strong>, apasionados por la tecnología y la innovación. Estamos aquí para ayudarte a
+          llevar tu negocio al siguiente nivel con soluciones de IA personalizadas.
+        </p>
+        <RouterLink to="/equipo"><button class="btn" onclick="this.blur()">Conoce al equipo</button></RouterLink>
       </div>
 
       <div>
